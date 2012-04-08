24ed6f300f2e75e4df3682a49381a527d231dc3e
Alexandre Blin <madjawa@gmail.com>
2012-04-08 18:00:28 +0200

    add first draft of parser

---
+++ src/parser/parser.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import lxml.html as html
+import argparse
+import os
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input')
+    parser.add_argument('--title', '-t', help="the slideshow's title", default="Presentation")
+
+    args = parser.parse_args()
+    
+    f = open(args.input)
+    doc = html.parse(args.input)
+    
+    templateName = 'html5slides'
+    
+    mainTemplate = open(os.path.join('templates', templateName, 'main.html')).read()
+    slideTemplate = open(os.path.join('templates', templateName, 'slide.html')).read()
+    
+    mainTemplate = mainTemplate.replace('<!-- {{TITLE}} -->', args.title)
+    
+    allSlides = ''
+    
+    for slide in doc.xpath('/html/body/article'):
+        slideHTML = ''
+        for element in slide.xpath('*'):
+            slideHTML += html.tostring(element)
+        
+        allSlides += slideTemplate.replace('<!-- {{CONTENT}} -->', slideHTML)
+    
+    print mainTemplate.replace('<!-- {{SLIDES}} -->', allSlides)
+    
