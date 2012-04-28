0ebd89215364097be449e87c053bd7291a3fe6f2
Alexandre Blin <madjawa@gmail.com>
2012-04-29 01:40:58 +0200

    added editor toolbar

--- src/js/editor.js
+++ src/js/editor.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 
 	now.ready(function() {
 		var delay;
-		var myCodeMirror = CodeMirror.fromTextArea(document.getElementById("editor"), {
+		var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
 			mode: 'text/html',
 			lineNumbers: true,
 			lineWrapping: true,
@@ -18,8 +18,45 @@ $(document).ready(function(){
 			}
 		});
 
+		$(".toolbar").click(function() {
+			var tool = $(this).attr("title");
+
+			var newSelection = editor.getSelection();
+
+			if (tool == "img") {
+				var src = prompt("Enter the URL of the image");
+
+				if (!src) return;
+
+				newSelection = '<img src="' + src + '" />';
+			}
+			else if (tool == "a") {
+				var href = prompt("Enter the URL of the link");
+
+				if (!href) return;
+
+				var text;
+
+				if (editor.somethingSelected()) {
+					text = editor.getSelection();
+				}
+				else {
+					text = prompt("Enter the text of the link");
+				}
+					
+				if (!text) text = href;
+
+				newSelection = '<a href="' + href + '">' + text + '</a>';
+			}
+			else if (editor.somethingSelected()) {
+				newSelection = "<"+tool+">"+newSelection+"</"+tool+">";
+			}
+
+			editor.replaceSelection(newSelection);
+		});
+
 		function updatePreview() {
-			now.transform(myCodeMirror.getValue(), function(val) {
+			now.transform(editor.getValue(), function(val) {
 				var doc = $('#preview iframe')[0];
 				var win = doc.contentDocument || doc.contentWindow.document;
 
